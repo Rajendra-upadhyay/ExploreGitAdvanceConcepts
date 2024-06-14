c1877d9710a4d20939fd9ea603c032be47e41258
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2024-06-14 22:25:43 +0530

    Revert "added change3 in main"
    This reverts commit a05a641998873bca85685ee6ee85eb67e631864e.

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first line added while creating repo");
-        System.out.println("change 3 in main");
     }
 }
