87f74dbb4cff172bbec47c5082eddb0d0e5c636e
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2024-06-14 22:21:38 +0530

    added change 2 in main

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,5 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first line added while creating repo");
+        System.out.println("second change added in main");
     }
 }
