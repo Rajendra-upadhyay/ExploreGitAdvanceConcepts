4c71ac79dd9c5cb4899e1ac0c2556e8a6fdd03d1
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2024-06-15 16:48:44 +0530

    adding change 6 will do revert it after 2 more comm

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first line added while creating repo");
         System.out.println("change 4 directly in repo");
+        System.out.println("change 6 will do this revert after adding two more commit");
     }
 }
