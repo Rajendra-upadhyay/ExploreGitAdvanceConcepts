c8b6e6fd67d10c856e0599d3a8846ef0b2942b74
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2024-06-15 17:02:08 +0530

    done required changes

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,7 +2,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("first line added while creating repo");
         System.out.println("change 4 directly in repo");
-        System.out.println("change 6 will do this revert after adding two more commit");
         System.out.println("change 7");
         System.out.println("change 8");
     }
